5b1b6c2921060372ce59aaace2252a83016ee659
MatheusSC017 <matheussimao2101@gmail.com>
2024-09-16 07:07:33 -0300

    improving scraping to wait load all the page

--- extract/contacts.py
+++ extract/contacts.py
@@ -1,6 +1,10 @@
 import re
 import logging
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import TimeoutException
 from multiprocessing import Process, Queue
@@ -12,7 +16,7 @@ def process_link(link):
     logging.info(f"Processing {link}")
     driver = webdriver.Firefox()
     try:
-        emails, phones = extract_contacts_with_timeout(link, driver, timeout=15)
+        emails, phones = extract_contacts_with_timeout(link, driver, timeout=20)
         logging.info(f"Found {len(emails)} emails and {len(phones)} phones for {link}")
         return emails, phones
     except Exception as e:
@@ -22,7 +26,7 @@ def process_link(link):
         driver.quit()
 
 
-def extract_contacts_with_timeout(link, driver, timeout=15):
+def extract_contacts_with_timeout(link, driver, timeout=20):
     queue = Queue()
     process = Process(target=extract_contacts_worker, args=(link, driver, queue))
 
@@ -40,8 +44,22 @@ def extract_contacts_with_timeout(link, driver, timeout=15):
 
 def extract_contacts_worker(link, driver, queue):
     try:
-        driver.set_page_load_timeout(10)  # Set page load timeout to 10 seconds
+        driver.set_page_load_timeout(20)
         driver.get(link)
+
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        scroll_pause_time = 2
+
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(scroll_pause_time)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+
+            if new_height == last_height:
+                break
+            last_height = new_height
 
         html_content = driver.page_source
         soup = BeautifulSoup(html_content, 'html.parser')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from extract.search import search_and_extract_links
 from extract.contacts import process_link
 from transform.links import get_base_links
@@ -10,7 +11,9 @@ search_engine_id = os.environ.get("SEARCH_ENGINE_ID")
 extracted_links = search_and_extract_links(search_term, api_key, search_engine_id, 10)
 base_links = get_base_links(extracted_links)
 
-with open('contacts.txt', 'w') as file:
+with open('contacts.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Link', 'Emails', 'Phones'])
     for link in base_links:
         emails, phones = process_link(link)
-        file.write(f"{link},{emails},{phones}\n")
+        writer.writerow([link, emails, phones])
