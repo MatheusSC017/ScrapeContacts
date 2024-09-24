95597efc9038d6501bb66d3b1a297fddad5229c9
MatheusSC017 <matheussimao2101@gmail.com>
2024-09-24 07:26:21 -0300

    Including command line to start search process

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
 import csv
+import argparse
 from extract.search import search_and_extract_links
 from extract.contacts import process_link
 from transform.links import get_base_links
 
-search_term = "Energia solar"
+parser = argparse.ArgumentParser(
+    prog="ScrapContacts",
+    description="Scrap contacts info from page of a specific category"
+)
+parser.add_argument("search_term", help="Term used during the search for the links")
+
+args = parser.parse_args()
+
 api_key = os.environ.get("API_KEY")
 search_engine_id = os.environ.get("SEARCH_ENGINE_ID")
 
-extracted_links = search_and_extract_links(search_term, api_key, search_engine_id, 10)
+extracted_links = search_and_extract_links(args.search_term, api_key, search_engine_id, 10)
 base_links = get_base_links(extracted_links)
 
 with open('contacts.csv', 'w', newline='') as file:
