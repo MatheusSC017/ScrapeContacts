496cb1e96cde3437b39869f4021df1d2566a3d7c
MatheusSC017 <matheussimao2101@gmail.com>
2024-09-25 08:16:47 -0300

    Including new arguments, error handling and log to error

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import os
 import csv
 import argparse
+import logging
 from extract.search import search_and_extract_links
 from extract.contacts import process_link
 from transform.links import get_base_links
+
+
+logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser(
     prog="ScrapContacts",
     description="Scrap contacts info from page of a specific category"
 )
-parser.add_argument("search_term", help="Term used during the search for the links")
+parser.add_argument("search_term", type=str, help="Term used during the search for the links")
+parser.add_argument("-o", "--output", type=str, help="Output CSV file name", default="contacts.csv")
+parser.add_argument("-n", "--number", type=int, help="Number of results expected", default=10)
+
 
 args = parser.parse_args()
 
 api_key = os.environ.get("API_KEY")
 search_engine_id = os.environ.get("SEARCH_ENGINE_ID")
 
-extracted_links = search_and_extract_links(args.search_term, api_key, search_engine_id, 10)
+try:
+    extracted_links = search_and_extract_links(args.search_term, api_key, search_engine_id, args.number)
+except Exception as e:
+    logging.error(f"Error extracting links: {e}")
+    exit(1)
+
 base_links = get_base_links(extracted_links)
 
-with open('contacts.csv', 'w', newline='') as file:
+with open(args.output, 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Link', 'Emails', 'Phones'])
     for link in base_links:
-        emails, phones = process_link(link)
-        writer.writerow([link, emails, phones])
+        try:
+            emails, phones = process_link(link)
+            writer.writerow([link, emails, phones])
+        except Exception as e:
+            logging.error(f"Error processing link {link}: {e}")
