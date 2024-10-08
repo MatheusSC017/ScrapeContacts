60aff5baef3a71dea0ff4d23967f457aa9fdb2ff
MatheusSC017 <matheussimao2101@gmail.com>
2024-10-08 14:06:58 -0300

    Updating to get filename automaticaly

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,8 @@ from flask_restful import Api, Resource
 from src.etl import etl_contacts
 import asyncio
 import json
+import unicodedata
+import re
 
 
 def create_app():
@@ -18,8 +20,12 @@ class ETLContacts(Resource):
         if 'search_term' not in parameters.keys():
             return {}
 
+        nfkd_str = unicodedata.normalize('NFKD', parameters['search_term'])
+        no_accents = ''.join([c for c in nfkd_str if not unicodedata.combining(c)])
+        output_filename = re.sub(r'\s+', '_', no_accents).lower()
+
         contacts = asyncio.run(etl_contacts(parameters['search_term'],
-                                            parameters.get('output', 'contacts.csv'),
+                                            output_filename,
                                             parameters.get('number', 10),
                                             parameters.get('exclude', [])))
 
